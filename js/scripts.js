035911eec68859fd53bc993b5845c0cd675908c8
Matheus Formigari <86173896+matheusformigari@users.noreply.github.com>
2021-09-19 16:46:24 -0300

    implementação da função que da inicio a sequência de cores, repetindo caso não haja interação do jogador

--- js/scripts.js
+++ js/scripts.js
@@ -71,6 +71,7 @@ _gui.pads.forEach(pad => {
 const startGame = () => {
 	blink("--", () => {
 		newColor()
+		playSequence()
 	})
 }
 
@@ -88,6 +89,40 @@ const newColor = () => {
 }
 
 const playSequence = () => {
+	let counter = 0,
+		padOn = true
+	
+	_data.playerSequence = []
+	_data.playerCanPlay = false
+
+	const interval = setInterval(() => {
+		if (!_data.gameOn) {
+			clearInterval(interval)
+			disablePads()
+			return
+		}
+		
+		if (padOn) {
+			if (counter === _data.gameSequence.length) {
+				clearInterval(interval)
+				disablePads()
+				waitForPlayerClick()
+				_data.playerCanPlay = true
+				return
+			}
+
+			const sndId = _data.gameSequence[counter]
+			const pad = _gui.pads[sndId]
+
+			_data.sounds[sndId].play()
+			pad.classList.add("game__pad--active")
+			counter++
+		} else {
+			disablePads()
+		}
+		padOn = !padOn
+	}, 750)
+
 
 }
 
@@ -103,7 +138,7 @@ const blink = (text, callback) => {
 			_gui.counter.classList.remove("gui__counter--on")
 			return
 		}
-		
+
 		if (on) {
 			_gui.counter.classList.remove("gui__counter--on")
 		} else {
@@ -120,7 +155,15 @@ const blink = (text, callback) => {
 }
 
 const waitForPlayerClick = () => {
+	clearTimeout(_data.timeout)
 
+	_data.timeout = setTimeout(() => {
+		if (!_data.playerCanPlay)
+		return
+
+	disablePads()
+	playSequence()
+	},5000)
 }
 
 const resetOrPlayAgain = () => {
